fbaffdeee5911024af489b4282e4ba8c07628957
sabina489 <Karkisabina869@gmail.com>
2022-07-05 07:24:31 +0545

    create student models

---
+++ database/migrations/2022_07_04_014938_create_students_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('name', 150);
+            $table->string('mobile, 10')->unique();
+            $table->string('email', 150)->unique();
+            $table->string('citizenship', 20)-> unique();
+            $table->string('gender', 1);
+            $table->string('perm-address', 150);
+            $table->string('temp-address', 150);
+            $table->date('dob', 150);
+            $table->string('blood-group', 3);
+            $table->boolean('is_active');
+            $table->boolean('is_almuni');
+            $table->string('picture', 20);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
